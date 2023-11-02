c8a2626d587e4bd8b30958e2a0b21d2f76a38f53
Yash <55951960+investyoung@users.noreply.github.com>
2023-11-02 12:50:58 -0400

    Create users.js

---
+++ users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// User registration route
+router.post('/register', (req, res) => {
+  // Implement user registration logic here
+});
+
+// Add more routes as needed
+
+module.exports = router;
