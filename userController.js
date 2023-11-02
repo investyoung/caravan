a5dc8b28d88b2fba35842ee2eb53cc1f84a87f97
Yash <55951960+investyoung@users.noreply.github.com>
2023-11-02 12:51:39 -0400

    Create userController.js

---
+++ userController.js
@@ -0,0 +1,12 @@
+// usersController.js
+
+// Function to handle user registration
+function registerUser(req, res) {
+  // Implement user registration logic here
+}
+
+// Export controller functions
+module.exports = {
+  registerUser,
+  // Add more controller functions as needed
+};
